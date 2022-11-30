2206dde7a638d7fa15dbbd91fc4ddcaed18dc15d
yejianbo <2662774551@qq.com>
2022-11-30 15:14:01 +0800

    First Commit

---
+++ src/main/java/cn/edu/njust/mhs/MhsApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.njust.mhs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MhsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MhsApplication.class, args);
+    }
+
+}
